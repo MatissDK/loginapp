5f8de1c0de88de5347ef2e905f6cb5a483fd120d
Otto Matiss <matiss.aprups@gmail.com>
2017-06-08 09:17:49 +0300

    Added mySQL

--- models/user.js
+++ models/user.js
@@ -1,46 +1,88 @@
-var mongoose = require('mongoose');
+var db = require('../db/connection');
 var bcrypt = require('bcryptjs');
 
-// User Schema
-var UserSchema = mongoose.Schema({
-	username: {
-		type: String,
-		index:true
-	},
-	password: {
-		type: String
-	},
-	email: {
-		type: String
-	},
-	name: {
-		type: String
-	}
-});
 
-var User = module.exports = mongoose.model('User', UserSchema);
+var User = {
 
-module.exports.createUser = function(newUser, callback){
-	bcrypt.genSalt(10, function(err, salt) {
-	    bcrypt.hash(newUser.password, salt, function(err, hash) {
-	        newUser.password = hash;
-	        newUser.save(callback);
+createUser: (newUser, callback)=>
+{
+     bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            newUser.password = hash;
+            return db.query('insert into login (name, email, username, password) VALUE (?, ?, ?, ?)',[newUser.name, newUser.email, newUser.username, newUser.password],
+                callback);
+        });
+    });
+
+},
+
+getUserByUsername: (username, callback) =>
+{
+    return db.query('SELECT * FROM login WHERE username = ?', [username], callback);
+},
+
+getUserById:(id, callback)=>
+{
+    return db.query("SELECT * FROM login WHERE id = ? ",[id], callback);
+},
+
+comparePassword: (candidatePassword, hash, callback)=>
+{
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+            if(err) throw err;
+            callback(null, isMatch);
 	    });
-	});
-}
+    },
 
-module.exports.getUserByUsername = function(username, callback){
-	var query = {username: username};
-	User.findOne(query, callback);
-}
+};
 
-module.exports.getUserById = function(id, callback){
-	User.findById(id, callback);
-}
+module.exports = User;
 
-module.exports.comparePassword = function(candidatePassword, hash, callback){
-	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
-    	if(err) throw err;
-    	callback(null, isMatch);
-	});
-}
+
+
+// var mongoose = require('mongoose');
+// var bcrypt = require('bcryptjs');
+
+// User Schema
+// var UserSchema = mongoose.Schema({
+// 	username: {
+// 		type: String,
+// 		index:true
+// 	},
+// 	password: {
+// 		type: String
+// 	},
+// 	email: {
+// 		type: String
+// 	},
+// 	name: {
+// 		type: String
+// 	}
+// });
+
+// var User = module.exports = mongoose.model('User', UserSchema);
+
+// module.exports.createUser = function(newUser, callback){
+// 	bcrypt.genSalt(10, function(err, salt) {
+// 	    bcrypt.hash(newUser.password, salt, function(err, hash) {
+// 	        newUser.password = hash;
+// 	        newUser.save(callback);
+// 	    });
+// 	});
+// }
+//
+// module.exports.getUserByUsername = function(username, callback){
+// 	var query = {username: username};
+// 	User.findOne(query, callback);
+// }
+//
+// module.exports.getUserById = function(id, callback){
+// 	User.findById(id, callback);
+// }
+//
+// module.exports.comparePassword = function(candidatePassword, hash, callback){
+// 	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+//     	if(err) throw err;
+//     	callback(null, isMatch);
+// 	});
+// }
